17870138d47367a677fa6a6cb614ba1077102fe3
大森 龍人 <106291438+RyutoOhmori0414@users.noreply.github.com>
2023-10-25 01:11:08 +0900

    [add] Player周りの入力管理の追加

---
+++ Assets/Game/Input/CustomInputManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomInputManager
+{
+    /// <summary>インスタンス</summary>
+    private static CustomInputManager _instance = null;
+
+    public static CustomInputManager Instance
+    {
+        get
+        {
+            _instance ??= new CustomInputManager();
+            return _instance;
+        }
+    }
+
+    private CustomInputManager()
+    {
+        _playerInputActions = new PlayerInputAction();
+        _playerInputActions.Enable();
+    }
+
+    /// <summary>PlayerのInput周り</summary>
+    private PlayerInputAction _playerInputActions;
+    /// <summary>PlayerのInput周り</summary>
+    public PlayerInputAction PlayerInputActions => _playerInputActions;
+}
