a3bf7e1062f2cb5260be652dcc9beed6fa82f80e
MatubaraTooru <106291581+MatubaraTooru@users.noreply.github.com>
2023-10-28 18:35:17 +0900

    [add] ExternalLifeManager 作成

---
+++ Assets/Game/Outgame_LifeManagement/Scripts/ExternalLifeManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ExternalLifeManager : MonoBehaviour
+{
+    [SerializeField, Tooltip("プレイヤーのライフを保持・設定するための変数")]
+    private static int _life = 5000;
+
+    [SerializeField, Tooltip("インプットフィールド")]
+    private InputField _lifeInputField;
+
+    /// <summary>シーン間でライフの受け渡しを行うためのプロパティ</summary>
+    public static int Life { get => _life; set => _life = value; }
+
+    public void InputLife()
+    {
+        _life = int.Parse(_lifeInputField.text);
+    }
+}
