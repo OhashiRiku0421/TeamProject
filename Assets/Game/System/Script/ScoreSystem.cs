246dc8a50ca20f92dc32a3005958e86d64b1f9f3
RK <riku.kuu0421@gmail.com>
2023-11-13 16:04:55 +0900

    [add]タイマーとスコアを追加

---
+++ Assets/Game/System/Script/ScoreSystem.cs
@@ -0,0 +1,42 @@
+﻿using UniRx;
+
+public class ScoreSystem
+{
+
+    private float _clearTime;
+
+    private static int _playerLife;
+
+    private static int _betLife;
+
+    private ReactiveProperty<int> _itemCount = new();
+
+    public IReactiveProperty<int> ItemCount => _itemCount;
+
+    public static ScoreData Score { get; private set; }
+
+    /// <summary>
+    /// リザルトに必要なライフを静的な変数に保存しておく
+    /// </summary>
+    public void GameResult()
+    {
+        Score = new(_clearTime, _playerLife, _itemCount.Value, _betLife);
+    }
+
+    /// <summary>
+    /// リザルトの計算に必要なライフを保存しておく
+    /// </summary>
+    public void SetLife(int playerLife, int betLife)
+    {
+        _playerLife = playerLife;
+        _betLife = betLife;
+    }
+
+    /// <summary>
+    /// アイテムの獲得数をカウント
+    /// </summary>
+    public void GetItem()
+    {
+        _itemCount.Value++;
+    }
+}
---
+++ Assets/Game/System/Script/TimerSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+[System.Serializable]
+public class TimerSystem
+{
+    [SerializeField, Tooltip("タイマーの初期値")]
+    private float _maxTime = 60;
+
+    private ReactiveProperty<float> _timer = new();
+
+    public IReactiveProperty<float> Timer => _timer;
+
+    public void Start()
+    {
+        _timer.Value = _maxTime;
+        _timer.FirstOrDefault(x => x <= 0f).Subscribe(_ => Debug.Log("GameOver"));
+    }
+
+    public void Update()
+    {
+        _timer.Value -= Time.deltaTime;
+    }
+
+}
