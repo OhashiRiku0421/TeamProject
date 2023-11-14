737c0ee89c7d2cc9b68852f706088b2e60ed2325
MatubaraTooru <106291581+MatubaraTooru@users.noreply.github.com>
2023-11-14 17:28:00 +0900

    [fix]  不必要な変数などを削除

--- Assets/Game/Outgame_LifeManagement/Scripts/ResultUIController.cs
+++ Assets/Game/Outgame_LifeManagement/Scripts/ResultUIController.cs
@@ -3,26 +3,17 @@ using UnityEngine.UI;
 
 public class ResultUIController : MonoBehaviour
 {
-    [SerializeField, Tooltip("手に入れたライフの値を表示するテキスト")]
-    private Text _getLifeValueText = null;
+    private ScoreData scoreData = new();
 
-    [SerializeField, Tooltip("失ったライフの値を表示するテキスト")]
-    private Text _lostLifeValueText = null;
+    [SerializeField, Tooltip("手に入ったライフ")]
+    private Text _getLifeText;
 
-    [SerializeField, Tooltip("ボーナスライフの値を表示するテキスト")]
-    private Text _bonusLifeValueText = null;
-
-    [SerializeField, Tooltip("現在のライフの値を表示するテキスト")]
-    private Text _nowLifeValueText = null;
-
-    [SerializeField, Tooltip("リザルトマネージャー")]
-    private ResultManager _resultManager = null;
+    [SerializeField, Tooltip("クリアした時間")]
+    private Text _clearTimeText;
 
     void Start()
     {
-        _getLifeValueText.text = _resultManager.GetLife.ToString();
-        _lostLifeValueText.text = _resultManager.LostLife.ToString();
-        _bonusLifeValueText.text = _resultManager.BonusLife.ToString();
-        _nowLifeValueText.text = _resultManager.CurrentLife.ToString();
+        _getLifeText.text = scoreData.PLayerLife.ToString();
+        _clearTimeText.text = scoreData.ClearTime.ToString();
     }
 }
