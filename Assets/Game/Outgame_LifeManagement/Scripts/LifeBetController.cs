04559fd0cbce74259c9d77abc4cd62214c88358f
MatubaraTooru <106291581+MatubaraTooru@users.noreply.github.com>
2023-11-14 17:20:55 +0900

    [update] ベットするライフを現在の仕様に合うよう変更

--- Assets/Game/Outgame_LifeManagement/Scripts/LifeBetController.cs
+++ Assets/Game/Outgame_LifeManagement/Scripts/LifeBetController.cs
@@ -9,9 +9,10 @@ public class LifeBetController : MonoBehaviour
     [SerializeField, Tooltip("スライダーの値に掛ける数")]
     private int _betLife = 50;
 
+    /// <summary>体力の最大数を1000としてベットしたライフの残りをプレイヤーのライフにする</summary>
     public void UpdateLife()
     {
-        Debug.Log($"BetLife : {(int)_betSlider.value * _betLife}");
-        ExternalLifeManager.Life = (int)_betSlider.value * _betLife;
+        Debug.Log($"BetLife : {1000 - (int)_betSlider.value * _betLife}");
+        ExternalLifeManager.Life = 1000 - (int)_betSlider.value * _betLife;
     }
 }
