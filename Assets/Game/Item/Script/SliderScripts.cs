963fac538275add158c511746ab7deca9fca307b
青山研人 <106291245+KentoAoyama@users.noreply.github.com>
2023-11-15 12:28:28 +0900

    [update]アルファ版作業

--- Assets/Game/Item/Script/SliderScripts.cs
+++ Assets/Game/Item/Script/SliderScripts.cs
@@ -1,33 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class SliderScripts : MonoBehaviour
 {
-    [SerializeField] private InGameSystem _gameSystem = default;
-    
-    [SerializeField, Tooltip("����ɂ����鎞��")]
+    private InGameSystem _gameSystem = default;
+
+    [SerializeField, Tooltip("採取にかかる時間")]
     private float _collectTime = 2F;
 
-    public Image _geji = null;
-    public GameObject _pcone = null;
+    [SerializeField, Tooltip("ゲージのImage")]
+    private Image _geji = null;
 
-    public event System.Action ItemGetCallback = default;
-    
-    float max = 1f;
-    float min = 0.1f;
+    public event Action ItemGetCallback = default;
 
-    private float cup = 0.0f;
-    /// <summary>�o�ߎ���</summary>
     private float _elapsed = 0.0F;
 
     private bool _isCollecting = false;
 
     private void Start()
     {
-        _geji.fillAmount = 1f;
+        if (_geji != null)
+            _geji.fillAmount = 1f;
+
+        _gameSystem = FindObjectOfType<InGameSystem>().GetComponent<InGameSystem>();
+
+        if (_gameSystem == null)
+            Debug.LogError("InGameSystemが見つかりませんでした");
     }
 
     public void CollectStart()
@@ -39,24 +38,22 @@ public class SliderScripts : MonoBehaviour
     {
         _isCollecting = false;
     }
-    
+
     private void Update()
     {
         if (_isCollecting)
         {
-
             _elapsed += Time.deltaTime;
-            _geji.fillAmount = 1 - (_elapsed / _collectTime);
+
+            //CriAudioManager.Instance.SE.Play("SE", "SE_System_Item_Get");
 
             if (_elapsed > _collectTime)
             {
                 _gameSystem.ScoreSystem.GetItem();
-                
-                ItemGetCallback?.Invoke();
-                CriAudioManager.Instance.SE.Play("SE", "SE_System_Item_Get");
-                
+
+                // TODO：アイテム取得時のSEをストップする
+
                 Destroy(this.gameObject);
-                gameObject.SetActive(false);
             }
         }
         else
