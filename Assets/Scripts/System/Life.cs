6f8da38176115b56c17ec0d3c49ca806a6f6dbad
RK <riku.kuu0421@gmail.com>
2024-06-13 00:10:40 +0900

    [Update]フォルダの整理

---
+++ Assets/Scripts/System/Life.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Life
+{
+
+    public static int PlayerLife;
+
+    public static int BetLife;
+}
